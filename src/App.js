187606fb1c1c97f1f0b602a1d275820be73da4f1
bodimfm <152702496+bodimfm@users.noreply.github.com>
2025-03-08 05:43:38 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import AIAssessmentQuiz from './AIAssessmentQuiz';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AIAssessmentQuiz />
+    </div>
+  );
+}
+
+export default App;
